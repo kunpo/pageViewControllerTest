c7049b7b529ba25511cee983d9ad63e3fbfe58da
Cai zongwei <zwcai@800best.com>
2019-07-08 16:07:46 +0800

    一些修改

--- pageViewControllerTest/ViewController.m
+++ pageViewControllerTest/ViewController.m
@@ -21,7 +21,7 @@
 @property (nonatomic, strong) UIPageViewController *pageViewContol;
 @property (nonatomic, strong) NSArray<UIButton *> *btns;
 @property (nonatomic, strong) NSArray<UIViewController *> *subControllers;
-@property (nonatomic, assign) int currentIndex; //记录当前显示的页面
+@property (nonatomic, weak) UIViewController *currentVC; //记录当前显示的页面
 @property (nonatomic, assign) BOOL isGoForward; //标记是下一页还是上一页
 @property (nonatomic, assign) BOOL inAnimation; //标记切换时动画是否正在做，防止动画冲突
 
@@ -43,6 +43,7 @@
         [temp addObject:sub];
     }
     self.subControllers = [NSArray arrayWithArray:temp];
+    self.currentVC = temp.firstObject;
     /*
      UIPageViewControllerTransitionStylePageCurl 翻页动画
      UIPageViewControllerTransitionStyleScroll 滑动
@@ -74,8 +75,8 @@
     NSUInteger index = [self.subControllers indexOfObject:vc];
     if (index < self.btns.count) {
         self.btns[index].selected = YES;
-        self.currentIndex = (int)index;
     }
+    self.currentVC = vc;
 }
 
 - (IBAction)btnClick:(UIButton *)sender {
@@ -91,13 +92,15 @@
     if (index < self.subControllers.count) {
         UIViewController *vc = self.subControllers[index];
         UIPageViewControllerNavigationDirection direction = UIPageViewControllerNavigationDirectionForward;
-        if (index < self.currentIndex) {
+        NSUInteger currentIndex = [self.subControllers indexOfObject:self.currentVC];
+        if (index < currentIndex) {
             direction = UIPageViewControllerNavigationDirectionReverse;
         }
         __weak ViewController *weakSelf = self;
+        
         [self.pageViewContol setViewControllers:@[vc] direction:direction animated:YES completion:^(BOOL finished) {
             if (finished) {
-                weakSelf.currentIndex = (int)index;
+                weakSelf.currentVC = vc;
                 weakSelf.inAnimation = NO;
             }
         }];
@@ -108,23 +111,35 @@
 
 //下一个页面
 - (nullable UIViewController *)pageViewController:(nonnull UIPageViewController *)pageViewController viewControllerAfterViewController:(nonnull UIViewController *)viewController {
-    if (self.currentIndex == 3) {
+    NSUInteger currentIndex = [self.subControllers indexOfObject:self.currentVC];
+    if (currentIndex == 3) {
         return nil;
     }
-    int index = self.currentIndex + 1;
+    NSUInteger index = currentIndex + 1;
     UIViewController *vc = self.subControllers[index];
-    self.isGoForward = YES;
+    NSLog(@"下一个");
     return vc;
 }
 //上一个页面
 - (nullable UIViewController *)pageViewController:(nonnull UIPageViewController *)pageViewController viewControllerBeforeViewController:(nonnull UIViewController *)viewController {
-    if (self.currentIndex == 0) {
+    NSUInteger currentIndex = [self.subControllers indexOfObject:self.currentVC];
+    if (currentIndex == 0) {
         return nil;
     }
-    int index = self.currentIndex - 1;
+    NSUInteger index = currentIndex - 1;
     UIViewController *vc = self.subControllers[index];
-    self.isGoForward = NO;
+    NSLog(@"上一个");
     return vc;
+}
+
+- (void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray<UIViewController *> *)pendingViewControllers {
+    NSUInteger index = [self.subControllers indexOfObject:pendingViewControllers.firstObject];
+    NSUInteger currentIndex = [self.subControllers indexOfObject:self.currentVC];
+    if (index > currentIndex) {
+        self.isGoForward = YES;
+    } else {
+        self.isGoForward = NO;
+    }
 }
 
 - (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray<UIViewController *> *)previousViewControllers transitionCompleted:(BOOL)completed {
@@ -132,19 +147,22 @@
      previousViewControllers 是上一次显示的vc。
      'finished'参数指示动画是否完成，而'completed'参数指示转换是否完成或退出(如果用户提前退出)。
      */
+    NSLog(@"完成");
     if ((completed == NO) || (finished == NO)) {
         return;
     }
     NSUInteger index = 0;
+    NSUInteger currentIndex = [self.subControllers indexOfObject:self.currentVC];
     if (self.isGoForward == YES) {
-        index = self.currentIndex + 1;
+        index = currentIndex + 1;
     } else {
-        index = self.currentIndex - 1;
+        index = currentIndex - 1;
     }
     if (index < self.subControllers.count) {
         UIViewController *currentVC = self.subControllers[index];
         [self selectViewController:currentVC];
     }
+    self.inAnimation = NO;
 }
 
 @end
